4859d0af46c5176e27318be544e582da65402af9
praveendwivedi <praveen240903@gmail.com>
2024-11-15 11:18:39 +0530

    setting upm the environment

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-zinc-500 text-white'>New Modified chat app</h1>
+    </>
+  )
+}
+
+export default App
